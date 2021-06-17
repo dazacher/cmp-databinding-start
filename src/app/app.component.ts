abace983eb5f41f61937c5e47879086593b2d83c
dazacher <dazacher2000@yahoo.com>
2021-06-17 06:45:51 -0700

    Using an Emitter Event and @Output decorator to bind button presses.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Test Server', content: 'Just a Test!'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintrData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintrData.serverName,
+      content: blueprintrData.serverContent
+    });
+  }
 }
